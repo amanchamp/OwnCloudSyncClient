0d220f0915b84a2c0f2a32f9e1e9311b858b4d81
Amandeep Panesar <amandeep@Amandeeos-MBP.home>
2017-07-31 21:23:29 -0400

    Copies Directory Structure to Remote Server

---
+++ FileSyncManager.py
@@ -0,0 +1,43 @@
+import ListDir as fl
+import ownCloudManager as manager
+
+
+def removeEmpty(array):
+    parsed = []
+    for a in array:
+        if not a == '':
+            parsed.append(a)
+    return parsed;
+
+
+
+def getTopLevel(f):
+    split = removeEmpty(f.split('/'))
+  
+    return  split[len(split)-1];
+
+        
+
+files = fl.getFiles()
+print 'Files'
+print files
+directoryOwnCloud = ' '
+directoryLocal = ' '
+
+
+for f in files:
+    if f[0] == '/':
+        directoryLocal = f
+        directoryOwnCloud +=getTopLevel(f) + '/'
+        print 'LOCAL DIRECTORY:   ' + directoryLocal
+        #manager.createDir(directoryOwnCloud)
+    else:
+
+        print 'File Upload Local:  ' + directoryLocal + '/' + f
+        print 'File Upload Remote:  ' + directoryOwnCloud  + f
+
+
+print directoryOwnCloud
+
+
+
